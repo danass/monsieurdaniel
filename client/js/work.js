964722bead92344838fab5aa7176e5c90905baf6
Daniel Assayag <danass@protonmail.com>
2021-04-19 16:10:43 +0000

    orientation

--- client/js/work.js
+++ client/js/work.js
@@ -1,10 +1,16 @@
 Template.work.onCreated(function () { 
-
     this.curindex = new ReactiveVar(0)
-    Tracker.autorun(() => {
+    this.orientation = new ReactiveVar(screen.orientation.type.match(/\w+/)[0])
+    Tracker.autorun((i, e) => {
       this.worksdb = new ReactiveVar(db.find({type:"Works", 'fields.unpublished' : {$exists: false} })  )
+      let instance = this
+      screen.orientation.onchange = function (){
+        // logs 'portrait' or 'landscape'
+      instance.orientation.set(screen.orientation.type.match(/\w+/)[0])
+    }
     })
     this.dir = new ReactiveVar(1) 
+    
 })
 
 
@@ -12,7 +18,11 @@ Template.work.helpers({
 entries(workid) {
   return db.find({ 'fields.Work': workid }, { sort: { createdTime: Template.instance().dir.get() } });
 },
- currentworkid() {
+ requestedid() {
    return FlowRouter.getParam('id')
+ },
+ orientation() {
+   return Template.instance().orientation.get()
  }
+
 })
